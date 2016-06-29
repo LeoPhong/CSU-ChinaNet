2cab79f14550a694bea71a0ccaf2e854dbe04f66
LeoPhong <jack.feng.liu@gmail.com>
2016-06-29 20:08:12 +0800

    修复了一个bug

--- StartUp.py
+++ StartUp.py
@@ -10,10 +10,9 @@ import Encryption
 if __name__ == "__main__":
     user_id = input('请输入帐号：')
     user_passwd = '0' + Encryption.encrypted_pwd(input('请输入密码：'))
-    connection_status,bras_address,user_ip = NetworkOpt.getConnectionInfo()
     try:
         while(True):
-            connection_status,None,None = NetworkOpt.getConnectionInfo()
+            connection_status,bras_address,user_ip = NetworkOpt.getConnectionInfo()
             if connection_status == 302:
                 print('正在登录...')
                 NetworkOpt.login(user_id,user_passwd,bras_address,user_ip)
