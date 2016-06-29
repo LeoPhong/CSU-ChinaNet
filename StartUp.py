6b55624518b30965decbf1af72c3a81e907d9749
LeoPhong <jack.feng.liu@gmail.com>
2016-06-29 19:52:20 +0800

    加入了登出功能，当按下ctrl+c后登出

--- StartUp.py
+++ StartUp.py
@@ -10,14 +10,18 @@ import Encryption
 if __name__ == "__main__":
     user_id = input('请输入帐号：')
     user_passwd = '0' + Encryption.encrypted_pwd(input('请输入密码：'))
-    while(True):
-        connection_status,bras_address,user_ip = NetworkOpt.getConnectionInfo()
-        if connection_status == 302:
-            print('正在登录...')
-            NetworkOpt.login(user_id,user_passwd,bras_address,user_ip)
-        elif connection_status == 200:
-            print('在线中...')
-        else:
-            print('异常！')
+    try:
+        while(True):
+            connection_status,bras_address,user_ip = NetworkOpt.getConnectionInfo()
+            if connection_status == 302:
+                print('正在登录...')
+                NetworkOpt.login(user_id,user_passwd,bras_address,user_ip)
+            elif connection_status == 200:
+                print('在线中...')
+            else:
+                print('异常！')
 
-        time.sleep(5)
+            time.sleep(5)
+    except KeyboardInterrupt as e:
+        print('正在登出...')
+        logout(bras_address,user_ip)
